be6296e56564ede7ec13754d4fec6a09efce8e98
Francisco Metzger <francisco.metzger@gmail.com>
2023-10-25 09:15:59 +0200

    feat: add simple publisher

---
+++ apps/ros2/src/raros/raros/simple_publisher.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class SimplePublisher(Node):
+    def __init__(self):
+        super().__init__('simple_publisher')
+        self.get_logger().info('Simple Publisher started')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 1
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = f'Publishing some value: {self.i}'
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SimplePublisher()
+    rclpy.spin(node)
+    node.destroy_node()
