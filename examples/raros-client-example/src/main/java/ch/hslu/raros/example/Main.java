f78ff79fc15e578e38e58f2698f589a481ab1b52
Francisco Metzger <francisco.metzger@gmail.com>
2023-11-03 22:49:45 +0100

    chore: setup example java client project

---
+++ examples/raros-client-example/src/main/java/ch/hslu/raros/example/Main.java
@@ -0,0 +1,9 @@
+package ch.hslu.raros.example;
+
+import ch.hslu.raros.client.SomeClass;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(SomeClass.someMethod());
+  }
+}
