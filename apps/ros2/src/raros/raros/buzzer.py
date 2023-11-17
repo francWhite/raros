bad2457cb3c80b88dbd1b3cabbae67c35607f3c8
Francisco Metzger <francisco.metzger@gmail.com>
2023-11-17 01:37:28 +0100

    refactor(magnet): implement PlayTone service as action

--- apps/ros2/src/raros/raros/buzzer.py
+++ apps/ros2/src/raros/raros/buzzer.py
@@ -1,34 +1,48 @@
+from typing import Optional
+
 import rclpy
 import time
 import RPi.GPIO as GPIO
 
 from rclpy.node import Node
-from raros_interfaces.srv import PlayTone
+from rclpy.action import ActionServer
+from raros_interfaces.action import PlayTone
 
 
 class Buzzer(Node):
     def __init__(self):
         super().__init__('buzzer')
         self.get_logger().info('buzzer node started')
-        self.service = self.create_service(PlayTone, 'buzzer/play_tone', self.play_tone_callback)
+        self.action_server = ActionServer(self, PlayTone, 'buzzer/play_tone', self.play_tone_callback)
         self.pin = 33
-        self.buzzer = None
+        self.buzzer: Optional[GPIO.PWM] = None
 
     def setup_gpio(self):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pin, GPIO.OUT)
         self.buzzer = GPIO.PWM(self.pin, 1000)
 
-    def play_tone_callback(self, request, response):
-        self.get_logger().debug(f'received: "{request}"')
+    def play_tone_callback(self, goal_handle):
+        request = goal_handle.request
+        self.get_logger().debug(f'executing goal for request: "{request}"')
+
+        feedback_msg = PlayTone.Feedback()
+        feedback_msg.remaining = request.duration
 
         self.buzzer.ChangeFrequency(request.frequency)
         self.buzzer.start(50)
-        time.sleep(request.duration / 1000)
-        self.buzzer.stop()
 
-        response.success = True
-        return response
+        for i in range(0, request.duration, 100):
+            sleep_duration = 100 if request.duration - i > 100 else request.duration - i
+            time.sleep(sleep_duration / 1000)
+            feedback_msg.remaining -= sleep_duration
+            goal_handle.publish_feedback(feedback_msg)
+
+        self.buzzer.stop()
+        goal_handle.succeed()
+
+        result = PlayTone.Result()
+        return result
 
 
 def main(args=None):
