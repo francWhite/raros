e4c7c23fdeb9af17c2372b2c21d15c906ce1afdb
Francisco Metzger <francisco.metzger@gmail.com>
2023-11-18 19:35:58 +0100

    chore(micro_ros): add RoboStepper library

--- apps/micro_ros/src/main.cpp
+++ apps/micro_ros/src/main.cpp
@@ -5,76 +5,71 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
+#include "Stepper.h"
+#include "RoboStepper.h"
+
+
 #include <std_msgs/msg/int32.h>
 
-#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
-#error This example is only avaliable for Arduino framework with serial transport.
-#endif
+RoboStepper roboStepper;
 
-rcl_publisher_t publisher;
+rcl_subscription_t subscriber;
 std_msgs__msg__Int32 msg;
-
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
 rcl_timer_t timer;
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+#define LED_PIN 13
 
-// Error handle loop
-void error_loop() {
-  while(1) {
-    delay(100);
-  }
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){return temp_rc;}}
+
+[[noreturn]]
+void error_loop(){
+    while(true){
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        delay(100);
+    }
 }
 
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-    msg.data++;
-  }
+void subscription_callback(const void * msgin)
+{
+    const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+    digitalWrite(LED_PIN, (msg->data == 0) ? LOW : HIGH);
 }
 
 void setup() {
-  // Configure serial transport
-  Serial.begin(115200);
-  set_microros_serial_transports(Serial);
-  delay(2000);
+    Serial.begin(115200);
+    set_microros_serial_transports(Serial);
 
-  allocator = rcl_get_default_allocator();
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
 
-  //create init_options
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+    delay(2000);
 
-  // create node
-  RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
+    allocator = rcl_get_default_allocator();
 
-  // create publisher
-  RCCHECK(rclc_publisher_init_default(
-    &publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "micro_ros_platformio_node_publisher"));
+    //create init_options
+    RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-  // create timer,
-  const unsigned int timer_timeout = 1000;
-  RCCHECK(rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
+    // create node
+    RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
 
-  // create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+    // create subscriber
+    RCCHECK(rclc_subscription_init_default(
+            &subscriber,
+            &node,
+            ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+            "micro_ros_arduino_subscriber"));
 
-  msg.data = 0;
+    // create executor
+    RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+    RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
 }
 
 void loop() {
-  delay(100);
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+    delay(100);
+    RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
