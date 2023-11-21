f247acb487db4131c9c10a15df73e9be787a5dee
Francisco Metzger <francisco.metzger@gmail.com>
2023-11-21 16:59:45 +0100

    feat(navigation): add endpoint for rotation

--- apps/api/src/routes/navigation.routes.ts
+++ apps/api/src/routes/navigation.routes.ts
@@ -7,24 +7,6 @@ import { ActionInvocationResult } from '../ros.model';
 export const navigationRouter = express.Router();
 navigationRouter.use(express.json());
 
-navigationRouter.post('/move', async (req: Request<undefined, undefined, MoveRequest>, res: Response) => {
-  console.log(req.originalUrl, req.body);
-
-  const requestData = { ...req.body };
-  rosService.callService(
-    '/raros/action_api/navigation/move',
-    'raros_interfaces/srv/ActionMove',
-    requestData,
-    (response: ActionInvocationResult) => {
-      const uidString = decode(response.goal_id.uuid);
-      console.log('received goal_id', uidString);
-
-      res.json({ goal_id: uidString }).send();
-    },
-    (error) => res.status(500).send(error),
-  );
-});
-
 navigationRouter.post('/stop', async (req: Request<undefined, undefined, undefined>, res: Response) => {
   console.log(req.originalUrl);
 
@@ -37,8 +19,45 @@ navigationRouter.post('/stop', async (req: Request<undefined, undefined, undefin
   );
 });
 
+navigationRouter.post('/move', async (req: Request<undefined, undefined, MoveRequest>, res: Response) => {
+  console.log(req.originalUrl, req.body);
+
+  const requestData = { ...req.body };
+  rosService.callService(
+    '/raros/action_api/navigation/move',
+    'raros_interfaces/srv/ActionMove',
+    requestData,
+    (response: ActionInvocationResult) => {
+      const uidString = decode(response.goal_id.uuid);
+      res.json({ goal_id: uidString }).send();
+    },
+    (error) => res.status(500).send(error),
+  );
+});
+
+navigationRouter.post('/rotate', async (req: Request<undefined, undefined, RotateRequest>, res: Response) => {
+  console.log(req.originalUrl, req.body);
+
+  const requestData = { ...req.body };
+  rosService.callService(
+    '/raros/action_api/navigation/rotate',
+    'raros_interfaces/srv/ActionRotate',
+    requestData,
+    (response: ActionInvocationResult) => {
+      const uidString = decode(response.goal_id.uuid);
+      res.json({ goal_id: uidString }).send();
+    },
+    (error) => res.status(500).send(error),
+  );
+});
+
 type MoveRequest = {
   distance: number;
   speed: number;
+  direction: number;
+};
+
+type RotateRequest = {
+  angle: number;
   direction: number;
 };
