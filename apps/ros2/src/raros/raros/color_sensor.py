91c3db6ffd211bfaa84bb7c147015bbbc1323e82
Francisco Metzger <francisco.metzger@gmail.com>
2023-12-15 22:59:06 +0100

    feat(configuration): add parameters for color_sensor node

--- apps/ros2/src/raros/raros/color_sensor.py
+++ apps/ros2/src/raros/raros/color_sensor.py
@@ -12,17 +12,30 @@ from std_msgs.msg import ColorRGBA
 class ColorSensor(Node):
     def __init__(self):
         super().__init__('color_sensor')
+        self.active, self.gain, self.integration_time = self.init_params()
+        if not self.active:
+            return
         self.get_logger().info('color_sensor node started')
+
         self.publisher = self.create_publisher(ColorRGBA, 'color_sensor/color', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
         self.i2c: Optional[I2C] = None
         self.sensor: Optional[TCS34725] = None
 
+    def init_params(self):
+        self.declare_parameter('active', True)
+        self.declare_parameter('gain', 16)
+        self.declare_parameter('integration_time', 120)
+        active = self.get_parameter('active').get_parameter_value().bool_value
+        gain = self.get_parameter('gain').get_parameter_value().integer_value
+        integration_time = self.get_parameter('integration_time').get_parameter_value().integer_value
+        return active, gain, integration_time
+
     def setup_i2c(self):
         self.i2c = board.I2C()
         self.sensor = TCS34725(self.i2c)
-        self.sensor.gain = 16
-        self.sensor.integration_time = 120
+        self.sensor.gain = self.gain
+        self.sensor.integration_time = self.integration_time
 
     def timer_callback(self):
         color_rgb = self.sensor.color_rgb_bytes
@@ -38,6 +51,9 @@ def main(args=None):
     rclpy.init(args=args)
 
     node = ColorSensor()
+    if not node.active:
+        node.get_logger().info('color_sensor node not active, exiting')
+        return
 
     try:
         node.setup_i2c()
