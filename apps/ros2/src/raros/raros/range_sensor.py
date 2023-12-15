974af0011d73d4b375c74264470927a31cdec8a4
Francisco Metzger <francisco.metzger@gmail.com>
2023-12-15 23:20:44 +0100

    feat(configuration): add parameters for range_sensor node

--- apps/ros2/src/raros/raros/range_sensor.py
+++ apps/ros2/src/raros/raros/range_sensor.py
@@ -1,8 +1,8 @@
 import math
 from typing import Optional
 
-import board
 import pwmio
+from adafruit_blinka.board.raspberrypi.raspi_4b import pin
 from adafruit_hcsr04 import HCSR04
 from adafruit_motor import servo
 
@@ -16,7 +16,12 @@ from rclpy.timer import Timer
 class RangeSensor(Node):
     def __init__(self):
         super().__init__('range_sensor')
+        (self.active, self.pwm_pin_servo, self.sensor_front_trigger_pin, self.sensor_front_echo_pin,
+         self.sensor_back_trigger_pin, self.sensor_back_echo_pin) = self.init_params()
+        if not self.active:
+            return
         self.get_logger().info('range_sensor node started')
+
         self.publisher = self.create_publisher(Distance, 'range_sensor/distance', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
         self.service = self.create_service(RotateRangeSensor, 'range_sensor/rotate', self.rotate_callback)
@@ -27,12 +32,29 @@ class RangeSensor(Node):
         self.sensor_back: Optional[HCSR04] = None
         self.rotate_cleanup_timer: Optional[Timer] = None
 
+    def init_params(self):
+        self.declare_parameter('active', True)
+        self.declare_parameter('pwm_pin_servo', 12)
+        self.declare_parameter('sensor_front.trigger_pin', 23)
+        self.declare_parameter('sensor_front.echo_pin', 24)
+        self.declare_parameter('sensor_back.trigger_pin', 8)
+        self.declare_parameter('sensor_back.echo_pin', 7)
+        active = self.get_parameter('active').get_parameter_value().bool_value
+        pwm_pin_servo = self.get_parameter('pwm_pin_servo').get_parameter_value().integer_value
+        sensor_front_trigger_pin = self.get_parameter('sensor_front.trigger_pin').get_parameter_value().integer_value
+        sensor_front_echo_pin = self.get_parameter('sensor_front.echo_pin').get_parameter_value().integer_value
+        sensor_back_trigger_pin = self.get_parameter('sensor_back.trigger_pin').get_parameter_value().integer_value
+        sensor_back_echo_pin = self.get_parameter('sensor_back.echo_pin').get_parameter_value().integer_value
+        return active, pwm_pin_servo, sensor_front_trigger_pin, sensor_front_echo_pin, sensor_back_trigger_pin, sensor_back_echo_pin
+
     def setup(self):
         # BCM numbering
-        self.pwm_servo = pwmio.PWMOut(pin=board.D12, frequency=50)
+        self.pwm_servo = pwmio.PWMOut(pin=pin.Pin(self.pwm_pin_servo), frequency=50)
         self.servo = servo.Servo(self.pwm_servo, min_pulse=600, max_pulse=2300, actuation_range=180)
-        self.sensor_front = HCSR04(trigger_pin=board.D23, echo_pin=board.D24)
-        self.sensor_back = HCSR04(trigger_pin=board.D8, echo_pin=board.D7)
+        self.sensor_front = HCSR04(trigger_pin=pin.Pin(self.sensor_front_trigger_pin),
+                                   echo_pin=pin.Pin(self.sensor_front_echo_pin))
+        self.sensor_back = HCSR04(trigger_pin=pin.Pin(self.sensor_back_trigger_pin),
+                                  echo_pin=pin.Pin(self.sensor_back_echo_pin))
 
     def cleanup(self):
         self.sensor_front.deinit()
@@ -70,6 +92,9 @@ def main(args=None):
     rclpy.init(args=args)
 
     node = RangeSensor()
+    if not node.active:
+        node.get_logger().info('range_sensor node not active, exiting')
+        return
 
     try:
         node.setup()
