6916e9e5b8c8cf6d30c943409f9c270c94bb48de
Francisco Metzger <francisco.metzger@gmail.com>
2023-12-15 22:06:26 +0100

    feat(configuration): add node parameters

--- apps/ros2/src/raros/raros/magnet.py
+++ apps/ros2/src/raros/raros/magnet.py
@@ -9,10 +9,20 @@ from std_srvs.srv import SetBool
 class Magnet(Node):
     def __init__(self):
         super().__init__('magnet')
+        self.active, self.pin = self.init_params()
+        if not self.active:
+            return
         self.get_logger().info('magnet node started')
+
         self.service = self.create_service(SetBool, 'magnet/set_state', self.set_magnet_state_callback)
         self.update_magnet_status_publisher = self.create_publisher(Bool, 'status/magnet_active', 10)
-        self.pin = 22
+
+    def init_params(self):
+        self.declare_parameter('active', True)
+        self.declare_parameter('pin', 22)
+        active = self.get_parameter('active').get_parameter_value().bool_value
+        pin = self.get_parameter('pin').get_parameter_value().integer_value
+        return active, pin
 
     def setup_gpio(self):
         GPIO.setmode(GPIO.BOARD)
@@ -32,6 +42,9 @@ def main(args=None):
     rclpy.init(args=args)
 
     node = Magnet()
+    if not node.active:
+        node.get_logger().info('magnet node not active, exiting')
+        return
 
     try:
         node.setup_gpio()
