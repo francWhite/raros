c0076d7ff62388e02f5df2c4ede6f91290b5d95c
Francisco Metzger <francisco.metzger@gmail.com>
2023-12-17 21:49:09 +0100

    chore(client): add publishing settings

--- apps/client/build.gradle.kts
+++ apps/client/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jreleaser.model.Active
+
 plugins {
   `java-library`
+  `maven-publish`
   signing
+  id("org.jreleaser") version "1.9.0"
 }
 
 group = "io.github.francwhite"
@@ -23,4 +27,70 @@ tasks.test {
 java {
   withSourcesJar()
   withJavadocJar()
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["java"])
+      pom {
+        packaging = "jar"
+        name = "Raros client library"
+        description = "Client library to interact with raros"
+        url = "https://github.com/francWhite/raros"
+        licenses {
+          license {
+            name = "MIT License"
+            url = "https://opensource.org/licenses/MIT"
+          }
+        }
+        developers {
+          developer {
+            id = "francWhite"
+            name = "Francisco Metzger"
+            email = "francisco.metzger@gmail.com"
+            url = "https://github.com/francWhite"
+          }
+        }
+        scm {
+          connection = "scm:git:git://github.com/francWhite/raros.git"
+          developerConnection = "scm:git:ssh://github.com/francWhite/raros.git"
+          url = "https://github.com/francWhite/raros"
+        }
+      }
+    }
+  }
+  repositories {
+    maven {
+      url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+    }
+  }
+}
+
+signing {
+  sign(publishing.publications["maven"])
+}
+
+jreleaser {
+  project {
+    copyright.set("Francisco Metzger")
+  }
+  gitRootSearch.set(true)
+  signing {
+    active.set(Active.ALWAYS)
+    armored.set(true)
+  }
+  deploy {
+    maven {
+      nexus2 {
+        create("maven-central") {
+          active.set(Active.ALWAYS)
+          url.set("https://s01.oss.sonatype.org/service/local")
+          closeRepository.set(false)
+          releaseRepository.set(false)
+          stagingRepositories.add("build/staging-deploy")
+        }
+      }
+    }
+  }
 }
