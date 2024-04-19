3d3922043d967cb508a59f8df940e1cbbf040a66
Francisco Metzger <francisco.metzger@gmail.com>
2024-04-19 10:12:24 +0200

    feat(navigation): add basic urdf model

--- apps/ros2/src/raros_navigation/setup.py
+++ apps/ros2/src/raros_navigation/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'raros_navigation'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'description'), glob('description/*.xacro'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
